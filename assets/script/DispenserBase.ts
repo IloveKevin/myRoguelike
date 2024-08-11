2f92940c4bb1e9b19cb4854437b162d22ae3dea9
Kevin-Z <kevin@kevin.com>
2024-08-11 20:34:23 +0800

    添加修改

--- assets/script/DispenserBase.ts
+++ assets/script/DispenserBase.ts
@@ -1,16 +1,21 @@
-import WeaponBase from "./WeaponBase";
+import WeaponControlBase from "./WeaponControlBase";
 
+/**
+ * 发射器基类
+ * 发射器的作用是按照规则发射武器
+ * 发射器持有一个武器管理器，武器管理器可以返回一个武器的实例
+ */
 export default class DispenserBase {
-    weapon: WeaponBase;
-
-    constructor(weapon: WeaponBase) {
-        let self = this;
-        self.weapon = weapon;
-    }
+    //这是按照规则发射出去的武器
+    weapon: WeaponControlBase;
 
     tryFire(): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    update(dt: number): void {
         let self = this;
 
-        return false;
+        self.weapon?.update(dt);
     }
 }
--- assets/script/WeaponBase.ts
+++ assets/script/WeaponBase.ts
@@ -1,10 +1,7 @@
 export default class WeaponBase {
-    //攻击力
-    attack: number;
-    //攻击范围
-    attackRange: number;
-    //攻击间隔
-    attackInterval: number;
-    //攻击冷却时间
-    attackCooldown: number;
+    node: cc.Node;
+
+    update(dt: number): void {
+        throw new Error("Method not implemented.");
+    }
 }
